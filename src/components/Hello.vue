16f1da58ad0c9fbd1cf63af7a82cc15f6b95a751
Xie Chenhong <xiech@garena.com>
2017-10-01 16:22:33 +0800

    vanilla

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>This is a demo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
